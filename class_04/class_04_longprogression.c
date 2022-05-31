0f15185c420c904b4cfcd9756f41c6ee9a7746d1
sueshin <sueshin@student.42seoul.kr>
2022-05-31 10:57:06 +0900

    .

--- class_04/class_04_longprogression.c
+++ class_04/class_04_longprogression.c
@@ -47,12 +47,70 @@ int	main()
 				max = temp;
 		}
 	}
-	idx = -1;
-	while (++idx < num)
-		printf("%d ", count[idx]);
-	printf("\n::::::::::::::::::\n");
-
 
 	printf("%d\n", max);
 	return (0);
 }
+
+/*
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int n, s[1001], biggest, c[1001];
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &s[i]);
+		c[i] = 1;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = (i - 1); j >= 1; j--) {
+			if ((s[j] < s[i]) && (c[j] >= c[i])) {
+				c[i] = c[j] + 1;
+			}
+		}
+	}
+	
+	biggest = c[1];
+
+	for (int i = 1; i <= n; i++)
+		if (c[i] > biggest)
+			biggest = c[i];
+
+	printf("%d\n", biggest);
+
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+#define MAX(n1, n2) n1 > n2 ? n1 : n2;
+
+int main()
+{
+    int N;
+    int result = 0;
+    int arr[1001];
+    int dp[1001] = {0, };
+    
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++)
+        scanf("%d", &arr[i]);
+    for (int cur = 1; cur <= N; cur++)
+    {
+        dp[cur] = 1;
+        for (int i = cur - 1; i >= 1; i--)
+        {
+            if (arr[cur] > arr[i])
+                dp[cur] = MAX(dp[cur], dp[i] + 1);
+        }
+        result = MAX(dp[cur], result);
+    }
+    printf("%d\n", result);
+}
+*/
