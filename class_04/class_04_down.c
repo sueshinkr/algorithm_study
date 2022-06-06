1d9294e744e8db88a37b10b40bd5684bdcbe28a8
sueshin <sueshin@student.42seoul.kr>
2022-06-06 15:59:43 +0900

    .

---
+++ class_04/class_04_down.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+
+int	main()
+{
+	int n, row, a, b, c, max[3], min[3], temp;
+	int	max_num, min_num;
+	
+	scanf("%d", &n);
+
+	row = -1;
+	while (++row < n)
+	{
+		scanf("%d %d %d", &a, &b, &c);
+		if (row == 0)
+		{
+			max[0] = a, min[0] = a;
+			max[1] = b, min[1] = b;
+			max[2] = c, min[2] = c;
+		}
+		else
+		{
+			temp = a > b ? a : b;
+			max[0] += temp;
+			temp = temp > c ? temp : c;
+			max[1] += temp;
+			temp = b > c ? b : c;
+			max[2] += temp;
+
+			temp = a < b ? a : b;
+			min[0] += temp;
+			temp = temp < c ? temp : c;
+			min[1] += temp;
+			temp = b < c ? b : c;
+			min[2] += temp;
+		}
+	}
+
+	max_num = max[0] > max[1] ? max[0] : max[1];
+	max_num = max_num > max[2] ? max_num : max[2];
+	min_num = min[0] < min[1] ? min[0] : min[1];
+	min_num = min_num < min[2] ? min_num : min[2];
+
+	printf("%d %d\n", max_num, min_num);
+}
+
+
+/*
+
+
+#include <stdio.h>
+
+int arr[100000][3], min[100000][3], max[100000][3];
+int	n, min_num, max_num;
+
+static void	dp_max(int row)
+{
+	int	temp;
+
+	if (row == n - 1)
+	{
+		temp = max[row][0] > max[row][1] ? max[row][0] : max[row][1];
+		temp = temp > max[row][2] ? temp : max[row][2];
+		max_num = temp;
+		return ;
+	}
+
+	temp = max[row][0] > max[row][1] ? max[row][0] : max[row][1];
+	max[row + 1][0] = temp + arr[row + 1][0];
+
+	temp = max[row][0] > max[row][1] ? max[row][0] : max[row][1];
+	temp = temp > max[row][2] ? temp : max[row][2];
+	max[row + 1][1] = temp + arr[row + 1][1];
+
+	temp = max[row][1] > max[row][2] ? max[row][1] : max[row][2];
+	max[row + 1][2] = temp + arr[row + 1][2];
+
+	dp_max(row + 1);
+}
+
+static void	dp_min(int row)
+{
+	int	temp;
+
+	if (row == n - 1)
+	{
+		temp = min[row][0] < min[row][1] ? min[row][0] : min[row][1];
+		temp = temp < min[row][2] ? temp : min[row][2];
+		min_num = temp;
+		return ;
+	}
+
+	temp = min[row][0] < min[row][1] ? min[row][0] : min[row][1];
+	min[row + 1][0] = temp + arr[row + 1][0];
+
+	temp = min[row][0] < min[row][1] ? min[row][0] : min[row][1];
+	temp = temp < min[row][2] ? temp : min[row][2];
+	min[row + 1][1] = temp + arr[row + 1][1];
+
+	temp = min[row][1] < min[row][2] ? min[row][1] : min[row][2];
+	min[row + 1][2] = temp + arr[row + 1][2];
+
+	dp_min(row + 1);
+}
+
+int	main()
+{
+	int row, column;
+	
+	scanf("%d", &n);
+
+	row = -1;
+	while (++row < n)
+	{
+		column = -1;
+		while (++column < 3)
+		{
+			scanf("%d", &arr[row][column]);
+			if (row == 0)
+			{
+				max[row][column] = arr[row][column];
+				min[row][column] = arr[row][column];
+			}
+			else
+			{
+				max[row][column] = -1;
+				min[row][column] = 1000001;
+			}
+		}
+	}
+
+	dp_max(0);
+	dp_min(0);
+
+	printf("%d %d\n", max_num, min_num);
+}
+
+*/
--- class_04/class_04_mincost.c
+++ class_04/class_04_mincost.c
@@ -1,41 +1,80 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX 2147483647
 
+struct to_cost
+{
+	int	to;
+	int	cost;
+};
+
 int	arr[1001][1001] = {0, };
 int	min_cost[1001] = {0, };
+struct to_cost	check[1000];
+
+static int compare (const void *a, const void *b)
+{
+	struct to_cost *one = (struct to_cost *)a;;
+	struct to_cost *two = (struct to_cost *)b;;
+
+	return (one->cost - two->cost); 
+}
+
+static void	set_mincost(int n, int from)
+{
+	int	to = 0, line = 0, idx = -1;
+
+	//printf("from : %d\n", from);
+	while(++to <= n)
+	{
+		if (arr[from][to] != -1 && min_cost[from] + arr[from][to] < min_cost[to])
+		{
+			//printf("%d %d\n", from, to);
+			min_cost[to] = min_cost[from] + arr[from][to];
+			check[line].to = to;
+			check[line++].cost = arr[from][to];
+		}
+	}
+	//우선순위 큐를 사용하는 방법도 나중에 시도해볼 것
+	if(line == 0)
+		return;
+	qsort(check, line - 1, sizeof(struct to_cost), compare);
+	//printf("sort\n");
+
+	while (++idx < line)
+		set_mincost(n, check[idx].to);
+}
+
 
 int	main()
 {
-	int	n, bus_n, from, to, cost;
+	int	n, bus_n, from, to, cost, final_from, final_to;
 
 	scanf("%d %d", &n, &bus_n);
+
+	from = 0;
+	while (++from <= n)
+	{
+		to = 0;
+		while (++to <= n)
+			arr[from][to] = -1;
+	}
 
 	while (bus_n-- > 0)
 	{
 		scanf("%d %d %d", &from, &to, &cost);
-		arr[from][to] = cost;
+		if (arr[from][to] != -1 && arr[from][to] > cost)
+			arr[from][to] = cost;
 		min_cost[to] = MAX;
 	}
 
-	to = 1;
-	while (++to <= n)
-	{
-		from = 0;
-		while (++from <= n)
-		{
-			if (arr[from][to] != 0)
-			{
-				cost = arr[from][to] + min_cost[from];
-				printf("arr[%d][%d] cost : %d\n", from, to, cost);
-				if (cost < min_cost[to])
-					min_cost[to] = cost;
-			}
-		}
-	}
+	scanf("%d %d", &final_from, &final_to);	
 
-	scanf("%d %d", &from, &to);
-	printf("%d\n", min_cost[to] - min_cost[from]);
+	min_cost[final_from] = 0;
+	set_mincost(n, final_from);
+	printf("%d\n", min_cost[final_to]);
 
 	return (0);
 }
