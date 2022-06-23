f0965f0ceb85ae7461dd533ed3409fb39ae26194
sueshin <sueshin@student.42seoul.kr>
2022-06-23 12:03:34 +0900

    .

--- class_04/class_04_breakwall.c
+++ class_04/class_04_breakwall.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#define max 1000*1000*2
+#include <stdbool.h>
+#define max 1001*1001*2
 
 struct que
 {
@@ -7,141 +8,76 @@ struct que
 	int dis, breakwall;
 };
 
-int	n, m, mindis = 1001;
-int	arr[1001][1001] = {0, };
-int	check[1001][1001][2] = {0, };
+int		n, m;
+int		arr[1001][1001] = {0, };
+bool	check[1001][1001][2] = {0, };
+struct que q[max] = {0, };
 
 static void	cal_mindis()
 {
-	int	row, column, dis, breakwall;
+	int	row, column, dis, breakwall, idx;
 	int	front = 0, rear = 1;
-	struct que q[max];
+	int	row_plus[] = {1, 0, -1, 0};
+	int	column_plus[] = {0, 1, 0, -1};
 
-	check[0][0][0] = 1;
-	check[0][0][1] = 1;
+	if (n == 1 && m == 1)
+	{
+		printf("1\n");
+		return ;
+	}
 
-	q[front].r = 0;
-	q[front].c = 0;
+	check[1][1][0] = 1;
+	check[1][1][1] = 1;
+
+	q[front].r = 1;
+	q[front].c = 1;
 	q[front].dis = 1;
 	q[front].breakwall = 0;
 
-	while (front != rear)
+	while (front < rear)
 	{
 		row = q[front].r;
 		column = q[front].c;
 		dis = q[front].dis;
 		breakwall = q[front++].breakwall;
-		front %= max;
+		//front %= max;
 
-		if (row == n - 1 && column == m - 1)
+		idx = -1;
+		while (++idx < 4)
 		{
-			printf("%d\n", dis);
-			return ;
-		}
+			if (row + row_plus[idx] >= 1 && row + row_plus[idx] <= n && column + column_plus[idx] >= 1 && column + column_plus[idx] <= m)
+			{
+				if (row + row_plus[idx] == n && column + column_plus[idx] == m)
+				{
+					printf("%d\n", dis + 1);
+					return ;
+				}
 
-		if (row > 0)
-		{
-			if (arr[row - 1][column] == 0)
-			{
-				if (check[row - 1][column][breakwall] == 0)
+				if (arr[row + row_plus[idx]][column + column_plus[idx]] == 0)
 				{
-					q[rear].breakwall = breakwall;
-					q[rear].r = row - 1;
-					q[rear].c = column;
-					q[rear++].dis = dis + 1;
-					check[row - 1][column][breakwall] = 1;
+					if (check[row + row_plus[idx]][column + column_plus[idx]][breakwall] == 0)
+					{
+						q[rear].breakwall = breakwall;
+						q[rear].r = row + row_plus[idx];
+						q[rear].c = column + column_plus[idx];
+						q[rear++].dis = dis + 1;
+						//rear %= max;
+						check[row + row_plus[idx]][column + column_plus[idx]][breakwall] = 1;
+					}
+				}
+				else if (arr[row + row_plus[idx]][column + column_plus[idx]] == 1)
+				{
+					if (breakwall == 0 && check[row + row_plus[idx]][column + column_plus[idx]][0] == 0)
+					{
+						q[rear].breakwall = 1;
+						q[rear].r = row + row_plus[idx];
+						q[rear].c = column + column_plus[idx];
+						q[rear++].dis = dis + 1;
+						//rear %= max;
+						check[row + row_plus[idx]][column + column_plus[idx]][1] = 1;
+					}
 				}
 			}
-			else if (arr[row - 1][column] == 1)
-			{
-				if (breakwall == 0 && check[row - 1][column][0] == 0)
-				{
-					q[rear].breakwall = 1;
-					q[rear].r = row - 1;
-					q[rear].c = column;
-					q[rear++].dis = dis + 1;
-					check[row - 1][column][1] = 1;
-				}
-			}
-			rear %= max;
-		}
-		if (row < n - 1)
-		{
-			if (arr[row + 1][column] == 0)
-			{
-				if (check[row + 1][column][breakwall] == 0)
-				{
-					q[rear].breakwall = breakwall;
-					q[rear].r = row + 1;
-					q[rear].c = column;
-					q[rear++].dis = dis + 1;
-					check[row + 1][column][breakwall] = 1;
-				}
-			}
-			else if (arr[row + 1][column] == 1)
-			{
-				if (breakwall == 0 && check[row + 1][column][0] == 0)
-				{
-					q[rear].breakwall = 1;
-					q[rear].r = row + 1;
-					q[rear].c = column;
-					q[rear++].dis = dis + 1;
-					check[row + 1][column][1] = 1;
-				}
-			}
-			rear %= max;
-		}
-		if (column > 0)
-		{
-			if (arr[row][column - 1] == 0)
-			{
-				if (check[row][column - 1][breakwall] == 0)
-				{
-					q[rear].breakwall = breakwall;
-					q[rear].r = row;
-					q[rear].c = column - 1;
-					q[rear++].dis = dis + 1;
-					check[row][column - 1][breakwall] = 1;
-				}
-			}
-			else if (arr[row][column - 1] == 1)
-			{
-				if (breakwall == 0 && check[row][column - 1][0] == 0)
-				{
-					q[rear].breakwall = 1;
-					q[rear].r = row;
-					q[rear].c = column - 1;
-					q[rear++].dis = dis + 1;
-					check[row][column - 1][1] = 1;
-				}
-			}
-			rear %= max;
-		}
-		if (column < m - 1)
-		{
-			if (arr[row][column + 1] == 0)
-			{
-				if (check[row][column + 1][breakwall] == 0)
-				{
-					q[rear].breakwall = breakwall;
-					q[rear].r = row;
-					q[rear].c = column + 1;
-					q[rear++].dis = dis + 1;
-					check[row][column + 1][breakwall] = 1;
-				}
-			}
-			else if (arr[row][column + 1] == 1)
-			{
-				if (breakwall == 0 && check[row][column + 1][0] == 0)
-				{
-					q[rear].breakwall = 1;
-					q[rear].r = row;
-					q[rear].c = column + 1;
-					q[rear++].dis = dis + 1;
-					check[row][column + 1][1] = 1;
-				}
-			}
-			rear %= max;
 		}
 	}
 	printf("-1\n");
@@ -154,11 +90,11 @@ int	main()
 
 	scanf("%d %d", &n, &m);
 
-	row = -1;
-	while (++row < n)
+	row = 0;
+	while (++row <= n)
 	{
-		column = -1;
-		while (++column < m)
+		column = 0;
+		while (++column <= m)
 			scanf("%1d", &arr[row][column]);
 	}
 
